01de5b9369f0f301a9cee91dd75f8ad299a0822a
jiniC <jinic323@gmail.com>
2018-02-04 23:21:22 +0900

    loading states

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -11,24 +11,6 @@ class App extends Component {
   // state바뀌면 새로운 state와 함께 바뀔 때마다 render()발생
   state = {
     greeting: "Hello!",
-    movies: [
-      {
-        title: "HarryPotter",
-        poster: "https://static.rogerebert.com/uploads/movie/movie_poster/harry-potter-and-the-sorcerers-stone-2001/large_uLGaJ9FgPWf7EUgwjp9RTmHemw8.jpg"
-      },
-      {
-        title: "Guardians of the Galaxy",
-        poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTAwMjU5OTgxNjZeQTJeQWpwZ15BbWU4MDUxNDYxODEx._V1_UY1200_CR90,0,630,1200_AL_.jpg"
-      },
-      {
-        title: "Zootopia",
-        poster: "https://images-na.ssl-images-amazon.com/images/I/71-Fj-WsM7L._SY550_.jpg"
-      },
-      {
-        title: "LalaLand",
-        poster: "https://media-cache.cinematerial.com/p/500x/4oa8qefc/la-la-land-slovak-movie-poster.jpg"
-      },
-    ]
   };
 
   componentDidMount() {
@@ -36,24 +18,38 @@ class App extends Component {
       this.setState({
         movies: [
           {
-            title: "Pirate of Caribbean",
-            poster:
-              "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYyMTcxNzc5M15BMl5BanBnXkFtZTgwOTg2ODE2MTI@._V1_UY1200_CR90,0,630,1200_AL_.jpg"
+            title: "HarryPotter",
+            poster: "https://static.rogerebert.com/uploads/movie/movie_poster/harry-potter-and-the-sorcerers-stone-2001/large_uLGaJ9FgPWf7EUgwjp9RTmHemw8.jpg"
           },
-          ...this.state.movies
+          {
+            title: "Guardians of the Galaxy",
+            poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTAwMjU5OTgxNjZeQTJeQWpwZ15BbWU4MDUxNDYxODEx._V1_UY1200_CR90,0,630,1200_AL_.jpg"
+          },
+          {
+            title: "Zootopia",
+            poster: "https://images-na.ssl-images-amazon.com/images/I/71-Fj-WsM7L._SY550_.jpg"
+          },
+          {
+            title: "LalaLand",
+            poster: "https://media-cache.cinematerial.com/p/500x/4oa8qefc/la-la-land-slovak-movie-poster.jpg"
+          },
         ]
       });
-    }, 1000);
+    }, 2000);
+  }
+
+  // react와 내 기능 차이두기위해서 '_'사용
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />;
+    })
+    return movies
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.movies.map((movie, index) => {
-          return (
-            <Movie title={movie.title} poster={movie.poster} key={index} />
-          );
-        })}
+        {this.state.movies ? this._renderMovies() : "Loading..."}
       </div>
     );
   }
