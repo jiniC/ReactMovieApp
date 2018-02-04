0eea6da89d58f7b4f110adfce7eea9a18a49bb8a
jiniC <jinic323@gmail.com>
2018-02-04 21:32:00 +0900

    state test

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -24,12 +24,34 @@ const movies = [
 
 // 부모가 데이터를 다 가지고 있음
 class App extends Component {
+  // Render: componentWillMount() -> render() -> componentDidMount()
+  // Update: componentWillReceiveProps() -> ShouldComponentUpdate() -> true -> componentWillUpdate() -> render() -> componentDidUpdate()
+
+  // state바뀌면 새로운 state와 함께 바뀔 때마다 render()발생
+  state = {
+    greeting: "Hello!"
+  };
+
+  componentDidMount() {
+    // mount 할때마다 greeting값 변경해서 render()다시 작동하겠음
+    setTimeout(() => {
+      // state값: 직접변경 불가 setState()이용
+      // this.state.greeting = 'something'
+      this.setState({
+        greeting: "Hellog again!"
+      });
+    }, 3000);
+  }
+
   render() {
     return (
       <div className="App">
+        {this.state.greeting}
         {movies.map((movie, index) => {
           // error: Each child in an array or iterator should have a unique "key" prop.
-          return <Movie title={movie.title} poster={movie.poster} key={index}/>;
+          return (
+            <Movie title={movie.title} poster={movie.poster} key={index} />
+          );
         })}
       </div>
     );
