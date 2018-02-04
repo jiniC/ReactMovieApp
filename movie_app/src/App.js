0e4cfb1fc6b20de313039fbd18db7ff294e81b98
jiniC <jinic323@gmail.com>
2018-02-04 17:13:26 +0900

    creating a movie app with temporary data - error correction

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -20,12 +20,14 @@ const movieImages = [
 // 부모가 데이터를 다 가지고 있음
 class App extends Component {
   render() {
-    return <div className="App">
+    return (
+      <div className="App">
         <Movie title={movieTitles[0]} poster={movieImages[0]} />
         <Movie title={movieTitles[1]} poster={movieImages[1]} />
         <Movie title={movieTitles[2]} poster={movieImages[2]} />
         <Movie title={movieTitles[3]} poster={movieImages[3]} />
-      </div>;
+      </div>
+    )
   };
 }
 
