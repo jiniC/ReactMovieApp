d54c211f1e7e6221114e19dff795415e0845ec3c
jiniC <jinic323@gmail.com>
2018-02-18 20:32:08 +0900

    creating a movie app with api - async,await

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -3,32 +3,45 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
-// 부모가 데이터를 다 가지고 있음
 class App extends Component {
   // Render: componentWillMount() -> render() -> componentDidMount()
   // Update: componentWillReceiveProps() -> ShouldComponentUpdate() -> true -> componentWillUpdate() -> render() -> componentDidUpdate()
 
   // state바뀌면 새로운 state와 함께 바뀔 때마다 render()발생
-  state = {}
+  state = {};
 
   componentDidMount() {
-    // promise: asynchronous, 비동기식, 첫 라인이 안끝나도 두번째 라인 작업, 계속 다른 작업 스케줄 해 둘 수 있음
-    // AJAX(=Asynchronous JavaScript And XML)
-    // fetch의 작업이 완료되면 -> then -> catch
-    fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
-      // response: fetch의 결과물
-      .then(response => response.json())
-      .then(json => console.log(json))
-      .catch(err => console.log(err));
+    this._getMovies();
   }
 
-  // react와 내 기능 차이두기위해서 '_'사용
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />;
+    const movies = this.state.movies.map(movie => {
+      // 컴포넌트의 key: 느림 -> id로 수정
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />;
+    });
+    return movies;
+  };
+  
+  // asyncronous function
+  _getMovies = async () => {
+    // await: this._callApi() 성공적완료가아닌 완료를 기다림, return value가 무엇이든 return value를 movies에 set
+    // const variable
+    // movies 값 = json.data.movies 값
+    const movies = await this._callApi()
+    
+    // this._callApi() 완료되기전 까지는(성공적 수행 no) 아래 코드 실행되지 않음
+    // 내 컴포넌트의 state를 movies로 설정
+    this.setState({
+      movies
     })
-    return movies
-  }
+  };
+
+  _callApi = () => {
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+      .then(response => response.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err))
+  };
 
   render() {
     return (
